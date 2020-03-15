ae6a4366b865dad30fcbfe6ffe6ea9a217d6164d
Megaflan Dull <megaflan@outlook.es>
2020-03-15 16:25:44 +0100

    15/03 - Starting project...

---
+++ ViewModels/ViewModelBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ReactiveUI;
+
+namespace Misifu.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
